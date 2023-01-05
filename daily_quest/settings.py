4edd86e8021b2749d219c36a78e3089aadb96825
Jnaesi <jacobnasi@hotmail.com>
2023-01-05 17:50:23 +0000

    fixed issue with deployment

--- daily_quest/settings.py
+++ daily_quest/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get('SECERET_KEY')
+SECRET_KEY =  "django-insecure-z$mtqyc%a_k_o8xt^&&r@ri+bu(m&kfsb=!#umql!y4x5386x6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'base.apps.BaseConfig',
-    'base',
 
 ]
 
@@ -82,13 +81,19 @@ WSGI_APPLICATION = 'daily_quest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+ 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

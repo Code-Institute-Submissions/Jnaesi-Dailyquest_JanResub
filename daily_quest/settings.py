7a9f03934e7495cff5f9e201f8979e7b97138ec5
Jnaesi <jacobnasi@hotmail.com>
2023-01-05 17:29:51 +0000

    deployment commit

--- daily_quest/settings.py
+++ daily_quest/settings.py
@@ -11,21 +11,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z$mtqyc%a_k_o8xt^&&r@ri+bu(m&kfsb=!#umql!y4x5386x6'
+SECRET_KEY =  os.environ.get('SECERET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dailyquests.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -36,8 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'base.apps.BaseConfig',
+    'base',
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +63,7 @@ ROOT_URLCONF = 'daily_quest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ env.py
@@ -0,0 +1,5 @@
+import os 
+
+os.environ["DATABASE_URL"]="postgres://sxgefnpj:iyxS3d4vn3NQwZ7QvLWwTxwdLrTgrN8a@manny.db.elephantsql.com/sxgefnpj"
+os.environ["SECRET_KEY"]="lol123"
+os.environ["CLOUDINARY_URL"]="cloudinary://546658217577574:JmORVr_Ka-Jq0mKM5aIeqjL5cz8@ddj06thqn"
